1b5cfc8a0155452609eb78954d85acfa24e59b17
Felix Lerner <felix.lerner01@gmail.com>
2024-03-09 17:27:45 -0300

    feat: add gradient

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ function NavBar() {
     return (
         <div>
             <div className="flex flex-col mx-auto bg-gray-200">
-                <aside className="group/sidebar flex flex-col items-center shrink-0 lg:w-[320px] w-[288px] transition-all duration-300 ease-in-out m-0 fixed z-40 inset-y-0 left-0 bg-slate-800 border-r border-r-dashed border-r-neutral-200 sidenav fixed-start loopple-fixed-start">
+                <aside className="group/sidebar flex flex-col items-center shrink-0 lg:w-[320px] w-[288px] transition-all duration-300 ease-in-out m-0 fixed z-40 inset-y-0 left-0 bg-gradient-to-b from-slate-900 to-slate-700 border-r border-r-dashed border-r-neutral-200 sidenav fixed-start loopple-fixed-start">
 
                     <div className="py-24 flex flex-col items-center">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg" className="w-44 h-44" />
