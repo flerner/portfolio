9b186f3575e55674cf47ec4375b0c9fac6328160
Felix Lerner <felix.lerner01@gmail.com>
2024-03-11 18:35:53 -0300

    feat: added home section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import Section from './components/Section'
+import backgroundImage from './assets/images/background.jpg'
 function App() {
 
 
   return (
     <div className='flex'>
       <NavBar />
-      <div className='flex flex-1 justify-center lg:ml-80 ml-72 transition-all duration-300 ease-in-out border border-black '><Section /></div>
+      <div className='flex flex-1 h-screen lg:ml-80 ml-72 transition-all duration-300 ease-in-out border border-black bg-gradient-to-r from-slate-950 to-slate-900'>
+        <Section />
+      </div>
 
     </div>
 
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,24 @@
+import { FaGithub } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
 function Section() {
     return (
-        <div>
-            <img src="https://cdn.pixabay.com/photo/2024/02/25/10/11/forsythia-8595521_1280.jpg" className="absolute inset-0 w-full h-full object-cover" />
+        <div className="flex flex-col ml-24 text-white ">
 
+            <h1 className="text-5xl lg:mt-48 ">Hello! I'm Felix Lerner</h1>
+            <p className="my-8 max-w-96 ml-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula iaculis volutpat. Quisque fringilla risus ligula, sit amet facilisis nisi pellentesque id ...</p>
+            <button className="max-w-44 focus:outline-none text-black text-xl bg-[#d9ff54] hover:bg-[#9fbd39] focus:ring-2 focus:ring-[#e3ecc2] font-medium rounded-lg py-2.5 my-6">Red</button>
+
+            <div className="flex my-10">
+                <IconContext.Provider value={{ color: "#d9ff54", size: "25px" }}>
+                    <a href="https://www.github.com/flerner"><FaGithub className="mx-4" /></a>
+                    <a href="https://www.linkedin.com/in/flerner"><FaLinkedinIn className="mx-4" /></a>
+                    <a href="mailto:felix.lerner01@gmail.com"><SiGmail className="mx-4" /></a>
+                </IconContext.Provider>
+
+            </div>
         </div>
 
     )
