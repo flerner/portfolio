e5fc8e7c927e48db714b73e34810ace50f2f7b70
Felix Lerner <felix.lerner01@gmail.com>
2024-03-12 15:01:47 -0300

    feat: added click to menu

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -1,23 +1,26 @@
+import { useState } from "react";
 import { IconContext } from "react-icons";
 import { IoMenu } from "react-icons/io5";
 
 function MobileNavBar() {
+    const [menuClicked, setMenuClicked] = useState(false)
+    const handleMenuClick = () => {
+        setMenuClicked(!menuClicked)
+    }
     return (
         <div className="h-screen">
             <IconContext.Provider value={{ size: "50px", color: "white" }}>
-                <IoMenu className="absolute z-10" />
+                <button className="absolute z-10" onClick={handleMenuClick}> <IoMenu /></button>
+
             </IconContext.Provider>
 
-            <div className='md:hidden absolute text-white flex flex-col pt-16 bg-black h-full opacity-80'>
-
-
+            {menuClicked && <div className='md:hidden absolute text-white flex flex-col pt-16 bg-black h-full opacity-80 transition-all duration-800'>
                 <button className=" text-white hover:border hover:border-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-8 text-center me-2 mb-2">Home</button>
                 <button className=" text-white hover:border hover:border-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-8 text-center me-2 mb-2">About</button>
                 <button className=" text-white hover:border hover:border-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-8 text-center me-2 mb-2">Skills</button>
                 <button className=" text-white hover:border hover:border-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-8 text-center me-2 mb-2">Work Experience</button>
                 <button className=" text-white hover:border hover:border-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-8 text-center me-2 mb-2">About</button>
-
-            </div>
+            </div>}
         </div>
 
     )
