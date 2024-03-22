e6916aad8ed6d15c5e8c6ee4f89587d407bb1a42
Felix Lerner <felix.lerner01@gmail.com>
2024-03-22 16:40:15 -0300

    feat: added github in projects, small improvements in home and work sections

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import NavOptions from "../NavOptions.js"
+import ProfilePhoto from "../assets/images/1685819963770.jpg"
 function NavBar({ onSelectOption }) {
     const handleClick = (option) => {
         onSelectOption(option)
@@ -9,7 +10,7 @@ function NavBar({ onSelectOption }) {
                 <aside className="group/sidebar flex flex-col items-center shrink-0 lg:w-[320px] md:w-[260px] transition-all duration-300 ease-in-out m-0 fixed z-40 inset-y-0 left-0 bg-gradient-to-b from-zinc-950 to-gray-950 border-r border-r-dashed border-r-neutral-200 sidenav fixed-start loopple-fixed-start">
 
                     <div className="py-24 flex flex-col items-center">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg" className="w-44 h-44" />
+                        <img src={ProfilePhoto} className="w-44 h-44 rounded-full" />
                         <h1 className="text-5xl tex py-10 text-white">Felix Lerner</h1>
                         <h3 className="text-white">React || Node || Mongo</h3>
                     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import NetflixImage from "../assets/images/netflix-clone.jpg"
+import { FaGithub } from "react-icons/fa";
 function Project() {
     return (
         <div class="max-w-sm bg-slate-900 border border-[#d9ff54] rounded-lg shadow ">
@@ -11,15 +12,15 @@ function Project() {
                 </a>
                 <p class="mb-3 font-normal text-white">A Netflix landing page, where you can play movie and tv shows trailers</p>
                 <div className="flex justify-between">
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    <a href="https://netflix-clone-gules-psi.vercel.app/" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                         Visit
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </a>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    <a href="https://github.com/flerner/netflix-clone" target="_blank" className="flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                         <span>GitHub</span>
-
+                        <FaGithub className="ml-2" />
                     </a>
 
                 </div>
