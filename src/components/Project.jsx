76507b8cc0380bd20be7de00e23fd5f71e224997
Felix Lerner <felix.lerner01@gmail.com>
2024-03-25 20:43:39 -0300

    feat: changed netflix-clone link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -12,7 +12,7 @@ function Project() {
                 </a>
                 <p class="mb-3 font-normal text-white">A Netflix landing page, where you can play movie and tv shows trailers</p>
                 <div className="flex justify-between">
-                    <a href="https://netflix-clone-gules-psi.vercel.app/" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    <a href="https://netflix-clone-rust-chi.vercel.app/" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                         Visit
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
