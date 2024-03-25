72b843a2a2a8581f9404322add70d9a6310b764f
Felix Lerner <felix.lerner01@gmail.com>
2024-03-25 19:55:44 -0300

    refactor: text

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -12,7 +12,7 @@ function HomeSection() {
             <div className="flex flex-col justify-center md:ml-24 text-white ">
 
                 <h1 className="text-5xl md:mt-48 ">Hello! I'm Felix Lerner</h1>
-                <p className="my-8 max-w-96 ml-1">Hello! I'm a Full Stack Developer based in Argentina. Welcome to my portfolio, where you can explore my skills, experiences, and projects.</p>
+                <p className="my-8 max-w-96 ml-1">I'm a Full Stack Developer based in Argentina. Welcome to my portfolio, where you can explore my skills, experiences, and projects.</p>
                 <a href={Resume} download="Felix Lerner - Resume" className="max-w-44 ">
                     <button className="max-w-44 focus:outline-none text-black text-xl bg-[#d9ff54] hover:bg-[#9fbd39] px-3 focus:ring-2 focus:ring-[#e3ecc2] font-medium rounded-lg py-2.5 my-6 flex items-center justify-center">Download CV<IoMdDownload className="mt-1 ml-2" /></button>
                 </a>
