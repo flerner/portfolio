a2525bbf262cec6bc85ae863b67693ff22c183bf
Felix Lerner <felix.lerner01@gmail.com>
2024-06-03 19:45:46 -0300

    project props

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,23 @@
 import NetflixImage from "../assets/images/netflix-clone.jpg"
 import { FaGithub } from "react-icons/fa";
-function Project() {
+function Project({ title, githubLink, link, image, description }) {
     return (
-        <div class="max-w-sm bg-slate-900 border border-[#d9ff54] rounded-lg shadow ">
+        <div className="max-w-sm bg-slate-900 border border-[#d9ff54] rounded-lg shadow ">
+            <h1 className="text-center text-red-900 text-lg p-3">{title}</h1>
             <a href="#">
-                <img class="rounded-t-lg" src={NetflixImage} alt="Netflix Clone" />
+                <img className="rounded-t-lg" src={image} alt="Netflix Clone" />
             </a>
-            <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                </a>
-                <p class="mb-3 font-normal text-white">A Netflix landing page, where you can play movie and tv shows trailers</p>
+            <div className="p-5">
+
+                <p className="mb-3 font-normal text-white">{description}</p>
                 <div className="flex justify-between">
-                    <a href="https://netflix-clone-rust-chi.vercel.app/" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    <a href={link} target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                         Visit
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </a>
-                    <a href="https://github.com/flerner/netflix-clone" target="_blank" className="flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    <a href={githubLink} target="_blank" className="flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-[#9fbd39] rounded-lg hover:bg-[#7d952f] focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                         <span>GitHub</span>
                         <FaGithub className="ml-2" />
                     </a>
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,5 @@
 import Project from "./Project"
+import NetflixImage from "../assets/images/netflix-clone.jpg"
 
 
 function ProjectsSection() {
@@ -6,9 +7,21 @@ function ProjectsSection() {
 
         <div className="flex flex-col justify-center m-auto mt-10 h-full md:h-screen">
             <div className="md:grid md:grid-cols-2 gap-14">
-                <Project />
-                <Project />
-                <Project />
+                <Project
+                    title="Netflix Clone"
+                    description="A frontend clone with the landing page of the most consumed streaming platform. Can reproduce trailers, if copyright allows, if not, a random video reagring that movie/tv show will reproduce"
+                    image={NetflixImage}
+                    githubLink="https://github.com/flerner/netflix-clone"
+                    link="https://streaming-app-clone.vercel.app/"
+                />
+                <Project
+                    title="Airbnb Clone"
+                    description="A fullstack Airbnb clone, with register, log in with google/github where you can create, reservate and see properties published. Made with Nextjs, MongoDB, Prisma, Nextauth"
+                    image={NetflixImage}
+                    githubLink="https://github.com/flerner/airbnb-clone"
+                    link="https://rent-app-clone-iota.vercel.app/"
+                />
+
                 <Project />
             </div>
         </div>
